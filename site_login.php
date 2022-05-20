e6d558d4627024cde6a155aeb89b38326930641c
WilderMO <wildermo2004@gmail.com>
2022-05-20 11:12:21 +0200

    sqtupc01

---
+++ site_login.php
@@ -0,0 +1,44 @@
+<?php
+
+include "connect.php";
+
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>TEST</title>
+
+    <link rel="stylesheet" href="css/site_login.css">
+</head>
+
+<body>
+
+    <header>
+        <nav>
+
+        </nav>
+    </header>
+
+    <section>
+        <center>
+            <div>
+                <input>
+                    <input type="text" name="username" id="username" placeholder="Username"> <br>
+                    <input type="email" name="email" id="email" placeholder="E-mail"> <br>
+                    <input type="email" name="repemail" id="repemail" placeholder="Repeat E-Mail"> <br>
+                    <input type="password" name="password" id="password" placeholder="Password"> <br>
+                    <input type="password" name="reppassword" id="reppassword" placeholder="Repeat Password"> <br>
+                </form>
+            </div>
+        </center>
+    </section>
+
+    <footer>
+
+    </footer>
+
+</body>
+
+</html>
---
+++ site_register.php
@@ -0,0 +1,38 @@
+<?php
+
+include "connect.php";
+
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>TEST</title>
+
+    <link rel="stylesheet" href="css/site_register.css">
+</head>
+
+<body>
+
+    <header>
+        <nav>
+
+        </nav>
+    </header>
+
+    <section>
+        <center>
+            <div>
+                
+            </div>
+        </center>
+    </section>
+
+    <footer>
+
+    </footer>
+
+</body>
+
+</html>
